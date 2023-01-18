45e3b1b2722766b9d36c52a4a81bd5a7b86de7c1
George <gwmkwan@gmail.com>
2023-01-18 08:20:51 -0700

    style: add NarBar styling

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
 import React from 'react';
 
+import NavLogo from '../public/assets/navLogo.png';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px16">
+        <image src="../public/assets/navLogo.png" alt="/" />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
