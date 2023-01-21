a563f211b89ee9ef3b880717e6daba8b071ecba0
George <gwmkwan@gmail.com>
2023-01-21 16:57:40 -0700

    style: add NavBar styling

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icon/ai';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const NavBar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between items-center w-full h-full px-2 2xl:px16">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <image
           src="/../public/assets/navLogo.png"
           alt="/"
@@ -37,6 +37,24 @@ const NavBar = () => {
           </ul>
           <div className="md:hidden">
             <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Image
+                src="/../public/assets/navLogo.png"
+                width="87"
+                height="35"
+                alt="/"
+              />
+              <div>
+                <AiOutlineClose />
+              </div>
+            </div>
           </div>
         </div>
       </div>
