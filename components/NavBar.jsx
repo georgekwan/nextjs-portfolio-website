ad3d025962df7deb8e261d4eb93d77cf3efaef4c
George <gwmkwan@gmail.com>
2023-01-21 17:59:51 -0700

    fix: correct color input

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -78,7 +78,7 @@ const NavBar = () => {
             </div>
             <div className="border-b border-gray-300 my-4">
               <p className="w-[85%] md:w-[90%] py-4">
-                Let's build something amazing together
+                Let&#39;s build something amazing together
               </p>
             </div>
           </div>
@@ -101,8 +101,8 @@ const NavBar = () => {
               </Link>
             </ul>
             <div className="pt-40">
-              <p className="uppercase tracking-widest text-#5651e5">
-                Let's Connect
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let&#39;s Connect
               </p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
