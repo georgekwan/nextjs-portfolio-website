3f5a5e4b9ef191dba12f2622814c0eadb94773fd
George <gwmkwan@gmail.com>
2023-01-21 17:13:23 -0700

    style: add width and padding to paragraph

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -57,7 +57,9 @@ const NavBar = () => {
               </div>
             </div>
             <div className="border-b border-gray-300 my-4">
-              <p>Let's build something amazing together</p>
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let's build something amazing together
+              </p>
             </div>
           </div>
         </div>
