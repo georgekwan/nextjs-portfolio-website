641bf367077d5d985e415b555f05b2c7c5f8bddf
George <gwmkwan@gmail.com>
2023-01-21 17:30:20 -0700

    style: add mouse style

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -86,7 +86,7 @@ const NavBar = () => {
                 Let's Connect
               </p>
               <div>
-                <div className="rounded-full shadow-lg shadow-gray-400">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
                   <FaLinkedinIn />
                 </div>
               </div>
