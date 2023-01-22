b51ac4a0de65335dbde740907c77777a520e8629
George <gwmkwan@gmail.com>
2023-01-21 17:34:09 -0700

    fix: correct typo

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -85,7 +85,7 @@ const NavBar = () => {
               <p className="uppercase tracking-widest text-#5651e5">
                 Let's Connect
               </p>
-              <div className="flex item-center justify-between my-4 w-full sm:w-[80%]">
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
                   <FaLinkedinIn />
                 </div>
