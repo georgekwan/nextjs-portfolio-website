90e5111d1972838d1d850d42059412f558135489
George <gwmkwan@gmail.com>
2023-01-21 17:17:30 -0700

    feat: add list items

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -67,6 +67,18 @@ const NavBar = () => {
               <Link href="/">
                 <li className="py-4 text-sm">Home</li>
               </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
             </ul>
           </div>
         </div>
