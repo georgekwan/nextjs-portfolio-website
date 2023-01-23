4b83b106c695b9d28093df791e3f7c029901182f
George <gwmkwan@gmail.com>
2023-01-23 11:51:27 -0700

    feat: add contact icons

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { FaLinkedinIn } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { AiOutlineMail } from 'react-icons/ai';
 
 const Main = () => {
   return (
@@ -21,8 +23,17 @@ const Main = () => {
             adaptable and ready for unexpected changes.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
               <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
             </div>
           </div>
         </div>
