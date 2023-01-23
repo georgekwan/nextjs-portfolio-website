3b7520f787e9a15547b6cf105f2286b915b04fb7
George <gwmkwan@gmail.com>
2023-01-22 21:43:30 -0700

    feat: add description and styling

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,18 @@ const Main = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET'S WORK TOGETHER TO BUILD SOMETHING
+            LET&#39;S WORK TOGETHER TO BUILD SOMETHING
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I&#39;m <span className="text-[#5651e5]"> George</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A Full-Stack Developer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            Meet George, an ambitious and friendly full-stack developer with a
+            background in project management. He approaches life with a "Live
+            Life Like A Movie" attitude, meaning he takes risks, follows his
+            curiosity, and creating an interesting and fulfilling life. He is
+            adaptable and ready for unexpected changes.
           </p>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import NavBar from '@/components/NavBar';
+import Main from '@/components/Main';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/fav.png" />
       </Head>
       <NavBar />
+      <Main />
     </>
   );
 }
