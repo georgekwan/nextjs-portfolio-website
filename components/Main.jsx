b6e4b162a011c090d2ef363f32ad9cafd7983a63
George <gwmkwan@gmail.com>
2023-01-23 12:09:36 -0700

    style: change text color

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,14 +8,14 @@ const Main = () => {
     <div className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">
+          <p className="uppercase text-sm tracking-widest text-[#334756]">
             LET&#39;S WORK TOGETHER TO BUILD SOMETHING
           </p>
-          <h1 className="py-4 text-gray-700">
-            Hi, I&#39;m <span className="text-[#cf3550]"> George</span>
+          <h1 className="py-4 text-[#2C394B]">
+            Hi, I&#39;m <span className="text-[#FF4C29]"> George</span>
           </h1>
-          <h1 className="py-2 text-gray-700">A Full-Stack Developer</h1>
-          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+          <h1 className="py-2 text-[#2C394B]">A Full-Stack Developer</h1>
+          <p className="py-4 text-[#334756] sm:max-w-[70%] m-auto">
             Meet George, an ambitious and friendly full-stack developer with a
             background in project management. He approaches life with a "Live
             Life Like A Movie" attitude, meaning he takes risks, follows his
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -103,7 +103,7 @@ const NavBar = () => {
               </Link>
             </ul>
             <div className="pt-40">
-              <p className="uppercase tracking-widest text-[#cf3550]">
+              <p className="uppercase tracking-widest text-[#FF4C29]">
                 Let&#39;s Connect
               </p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
