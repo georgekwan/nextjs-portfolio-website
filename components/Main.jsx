e3c0da42c089f3c4727d7c60bcfdf8972a890747
George <gwmkwan@gmail.com>
2023-01-22 21:29:09 -0700

    style: paragraph styling

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,7 +4,11 @@ const Main = () => {
   return (
     <div className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div></div>
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET'S WORK TOGETHER TO BUILD SOMETHING
+          </p>
+        </div>
       </div>
     </div>
   );
