48ff945ddaeff8b460a80b57657f4099310e9781
George <gwmkwan@gmail.com>
2023-01-23 20:41:51 -0700

    feat: add paragraph and h2

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p>About</p>
+          <h2>Who I Am</h2>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
