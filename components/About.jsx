a2fc402adf24b1b6776191f40e076347530fdd0c
George <gwmkwan@gmail.com>
2023-01-23 20:45:45 -0700

    feat: add about me

--- components/About.jsx
+++ components/About.jsx
@@ -7,6 +7,37 @@ const About = () => {
         <div className="col-span-2">
           <p>About</p>
           <h2>Who I Am</h2>
+          <p>
+            Meet George, an ambitious and friendly full-stack developer with a
+            background in project management. He approaches life with a "Live
+            Life Like A Movie" attitude, meaning he takes risks, follows his
+            curiosity, and lives life on his own terms. He is adaptable and
+            ready for unexpected changes.
+          </p>
+          <p>
+            George has a deep-seated love of coding, just like someone who goes
+            to the gym regularly to lift weights. He has a keen eye for detail,
+            strong analytical skills, and a natural problem-solving ability,
+            which he developed from his background in indirect taxes. He is
+            always looking for new opportunities to improve his skills and grow
+            as a developer and is not afraid to put in the extra effort to
+            achieve success. With an organized and efficient approach, George
+            approaches tasks with purpose and precision, similar to someone who
+            is dedicated to hitting their fitness goals.
+          </p>
+          <p>
+            In his free time, George enjoys exploring new things, experiencing
+            different cultures, and having fun. He loves traveling and going on
+            adventures and is skilled at whitewater paddleboarding and
+            badminton. George believes in balancing work and play and creates a
+            positive and enjoyable work environment. He is committed to
+            motivating and supporting his team members in achieving success.
+            George is a hardworking and sincere individual who always looks for
+            ways to live life to the fullest. If you're interested in learning
+            more about George's skills and experience, don't hesitate to reach
+            out to him!
+          </p>
+          <p>Check out some of my latest projects.</p>
         </div>
         <div></div>
       </div>
