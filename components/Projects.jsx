99ea9df93403c3f3c61b5b57b90e8d90958f1dc2
George <gwmkwan@gmail.com>
2023-01-29 21:47:52 -0700

    feat: create project item container and styling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -10,7 +10,9 @@ const Projects = () => {
           Projects
         </p>
         <h2 className="py-4">What I've Built</h2>
-        <div className="grid md:grid-cols-2 gap-8"></div>
+        <div className="grid md:grid-cols-2 gap-8">
+          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#FF4C29] to-[#c35714fc]"></div>
+        </div>
       </div>
     </div>
   );
