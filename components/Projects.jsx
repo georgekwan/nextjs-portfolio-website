69a437333d305881a7aae09d7f9fe69fe747c7e5
George <gwmkwan@gmail.com>
2023-01-29 21:59:40 -0700

    feat: add project screenshot

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import propertyImg from '../public/assets/projects/property.jpg';
 
 const Projects = () => {
   return (
@@ -11,7 +12,9 @@ const Projects = () => {
         </p>
         <h2 className="py-4">What I've Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#FF4C29] to-[#c35714fc]"></div>
+          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#FF4C29] to-[#c35714fc]">
+            <Image src={propertyImg} alt="/"></Image>
+          </div>
         </div>
       </div>
     </div>
