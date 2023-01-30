78318e0d9ff55c8f312395b46b917bbff921992d
George <gwmkwan@gmail.com>
2023-01-29 21:25:49 -0700

    feat: add link

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const About = () => {
   return (
-    <div className="w-full md:h-screen p-2 flex items-center py-16">
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracking-widest text-[#FF4C29]">
