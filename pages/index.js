08076f44d46c7ef7d3853b485a9cc69bc89022e0
George <gwmkwan@gmail.com>
2023-01-29 21:03:02 -0700

    feat: create project component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import NavBar from '@/components/NavBar';
 import Main from '@/components/Main';
 import About from '@/components/About';
 import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Main />
       <About />
       <Skills />
+      <Projects />
     </>
   );
 }
