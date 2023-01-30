33394a55cc51e846399a009d2b3b5ba7f63c60c6
George <gwmkwan@gmail.com>
2023-01-29 21:37:36 -0700

    style: add text styling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,8 +6,10 @@ const Projects = () => {
   return (
     <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <p>Projects</p>
-        <h2>What I've Built</h2>
+        <p className="text-xl tracking-widest uppercase text-[#FF4C29]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've Built</h2>
         <div className="grid md:grid-cols-2 gap-8"></div>
       </div>
     </div>
