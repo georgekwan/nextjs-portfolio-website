eeb11b92ef9fb2acc24bd19926b52cdb5ddd1a1f
George <gwmkwan@gmail.com>
2023-01-29 21:26:19 -0700

    feat: add link and styling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p>Projects</p>
+        <h2>What I've Built</h2>
+        <div className="grid md:grid-cols-2 gap-8"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
