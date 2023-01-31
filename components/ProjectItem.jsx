e23de4df0ef95f4e109c416f9ebedb02cd41fc86
George <gwmkwan@gmail.com>
2023-01-30 21:50:23 -0700

    feat: move project item to a separate file

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const ProjectItem = () => {
-  return <div>ProjectItem</div>;
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#FF4C29] to-[#c35714fc]">
+      <Image
+        className="rounded-xl group-hover:opacity-10"
+        src={propertyImg}
+        alt="/"
+      />
+
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2x1 text-white tracking-wider text-center">
+          House Marketplace
+        </h3>
+        <p className="pb-4 pt-2 text-white text-center">React JS</p>
+        <Link href="/">
+          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+            More Info
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectItem;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import propertyImg from '../public/assets/projects/property.jpg';
+import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
@@ -12,25 +13,7 @@ const Projects = () => {
         </p>
         <h2 className="py-4">What I&apos;ve Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#FF4C29] to-[#c35714fc]">
-            <Image
-              className="rounded-xl group-hover:opacity-10"
-              src={propertyImg}
-              alt="/"
-            />
-
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-              <h3 className="text-2x1 text-white tracking-wider text-center">
-                House Marketplace
-              </h3>
-              <p className="pb-4 pt-2 text-white text-center">React JS</p>
-              <Link href="/">
-                <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
-                  More Info
-                </p>
-              </Link>
-            </div>
-          </div>
+          <ProjectItem />
         </div>
       </div>
     </div>
