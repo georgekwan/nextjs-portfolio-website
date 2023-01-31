b926f9c717a78e546114bd9bf63f7c8fa0ea4d59
George <gwmkwan@gmail.com>
2023-01-30 21:54:52 -0700

    feat: create project item properties

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -13,7 +13,31 @@ const Projects = () => {
         </p>
         <h2 className="py-4">What I&apos;ve Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem />
+          <ProjectItem
+            title="Property Finder"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+          />
+          <ProjectItem
+            title="Weather Based Activity App"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+          />
+          <ProjectItem
+            title="Github Finder"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+          />
+          <ProjectItem
+            title="Property Finder"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+          />
+          <ProjectItem
+            title="Property Finder"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+          />
         </div>
       </div>
     </div>
