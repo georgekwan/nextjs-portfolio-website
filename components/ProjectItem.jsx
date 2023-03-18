b7a8463114aa1faba53216ee117ac812ed84b0f3
George <gwmkwan@gmail.com>
2023-03-18 16:31:09 -0600

    feat: change project item to be reusable

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const ProjectItem = () => {
+const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#FF4C29] to-[#c35714fc]">
       <Image
         className="rounded-xl group-hover:opacity-10"
-        src={propertyImg}
+        src={backgroundImg}
         alt="/"
       />
 
       <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <h3 className="text-2x1 text-white tracking-wider text-center">
-          House Marketplace
+          {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">React JS</p>
-        <Link href="/">
+        <Link href={projectUrl}>
           <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
             More Info
           </p>
