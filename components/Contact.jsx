6d59ab27ac3fce1b19cb0279fc19cd7bc869b45f
George <gwmkwan@gmail.com>
2023-03-18 16:41:24 -0600

    style: add styling

---
+++ components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p>Contact</p>
+        <h2></h2>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
