650f30ec4eb6ef2ba13982cf76e4889f8818592c
George <gwmkwan@gmail.com>
2023-03-18 16:55:01 -0600

    feat: add image and styling

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,8 +4,23 @@ const Contact = () => {
   return (
     <div className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
-        <p>Contact</p>
-        <h2></h2>
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Contact
+        </p>
+        <h2 className="py-4">Get In Touch</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full">
+              <div>
+                <img
+                  className="rounded-xl hover:scale-105 ease-in duration-300"
+                  src="https://cdn.pixabay.com/photo/2014/11/25/16/56/hands-545394_960_720.jpg"
+                  alt="/"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
