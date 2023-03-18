47d07cc292744aa836c72c01f39b26ceda501b08
George <gwmkwan@gmail.com>
2023-03-18 17:29:16 -0600

    chore: add labels

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,6 +12,7 @@ const Contact = () => {
         </p>
         <h2 className="py-4">Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
+          {/* About Me */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full">
               <div>
@@ -48,7 +49,7 @@ const Contact = () => {
               </div>
             </div>
           </div>
-
+          {/* Contact */}
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
               <form>
