ef1f92863f75bd4473ca263c04f1183c78f1cde3
George <gwmkwan@gmail.com>
2023-03-18 17:12:14 -0600

    fix: connect with me position

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -29,21 +29,21 @@ const Contact = () => {
                   and let's talk.
                 </p>
               </div>
-            </div>
-            <div>
-              <p className="uppercase pt-8">Connect With Me</p>
-              <div className="flex item-center justify-between py-4">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaLinkedinIn />
-                </div>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaGithub />
-                </div>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <AiOutlineMail />
-                </div>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <BsFillPersonLinesFill />
+              <div>
+                <p className="uppercase pt-8">Connect With Me</p>
+                <div className="flex item-center justify-between py-4">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaGithub />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <AiOutlineMail />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <BsFillPersonLinesFill />
+                  </div>
                 </div>
               </div>
             </div>
