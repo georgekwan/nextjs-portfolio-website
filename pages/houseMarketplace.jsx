f8cc64a28a40562020ce5fb9ea97931e519706ff
George <gwmkwan@gmail.com>
2023-03-19 18:31:38 -0600

    feat: add links to buttons

--- pages/houseMarketplace.jsx
+++ pages/houseMarketplace.jsx
@@ -11,10 +11,9 @@ const houseMarketplace = () => {
       <div className="w-screen h-[50vh] relative">
         <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
         <Image
-          className="absolute z-1"
-          style={{ objectFit: 'cover' }}
-          fill
+          className="absolute z-1 object-cover"
           src={propertyImg}
+          fill
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
@@ -22,7 +21,7 @@ const houseMarketplace = () => {
           <h3>React JS / Firebase</h3>
         </div>
       </div>
-      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
         <div className="col-span-4">
           <p>Project</p>
           <h2>Overview</h2>
@@ -31,8 +30,20 @@ const houseMarketplace = () => {
             list their home or apartment for rent or for sale. They can also add
             it as a special offer.
           </p>
-          <button className="px-8 py-2 mt-4 mr-8">Demo</button>
-          <button className="px-8 py-2 mt-4">Code</button>
+          <a
+            href="https://house-marketplace-george-kwan.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          </a>
+          <a
+            href="https://github.com/georgekwan/house-marketplace"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4">Code</button>
+          </a>
         </div>
         <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
           <div className="p-2">
