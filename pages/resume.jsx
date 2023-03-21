a736f367b559fbced6d82c272bd5d29f471846cb
George <gwmkwan@gmail.com>
2023-03-21 11:38:46 -0600

    feat: add external links

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -13,6 +13,28 @@ const resume = () => {
         />
         <link rel="icon" href="/fav.png" />
       </Head>
+      <div>
+        <h2 className="text-center">Resume</h2>
+        <div className="bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center">
+          <h2 className="text-center">George Kwan</h2>
+          <div className="flex">
+            <a
+              href="https://www.linkedin.com/in/george-kwan/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedinIn size={20} style={{ marginRight: '1rem' }} />
+            </a>
+            <a
+              href="https://github.com/georgekwan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub size={20} style={{ marginRight: '1rem' }} />
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
