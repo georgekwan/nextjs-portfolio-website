6e09d747c2d179e5300d4f0831d3faf5e997a203
George <gwmkwan@gmail.com>
2023-03-21 11:15:51 -0600

    feat: create resume page

---
+++ pages/resume.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Head from 'next/head';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const resume = () => {
+  return <div>resume</div>;
+};
+
+export default resume;
