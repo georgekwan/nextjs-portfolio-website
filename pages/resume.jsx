291d327d8e8f3eedd772a58bca14d9340b9602f6
George <gwmkwan@gmail.com>
2023-03-21 11:35:33 -0600

    feat: add header

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -3,7 +3,18 @@ import Head from 'next/head';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const resume = () => {
-  return <div>resume</div>;
+  return (
+    <>
+      <Head>
+        <title>George | Resume</title>
+        <meta
+          name="description"
+          content="I am a full Stack Developer specializing in seamless web and mobile experiences."
+        />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+    </>
+  );
 };
 
 export default resume;
