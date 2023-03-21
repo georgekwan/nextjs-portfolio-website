96009510cdd3b839194b0bd0d89ef682f16c67ce
George <gwmkwan@gmail.com>
2023-03-21 11:34:23 -0600

    feat: add resume link

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -78,6 +78,9 @@ const NavBar = () => {
               <Link href="/#projects">Projects</Link>
             </li>
             <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/#resume">Resume</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
               <Link href="/#contact">Contact</Link>
             </li>
           </ul>
@@ -145,6 +148,11 @@ const NavBar = () => {
               <Link href="/#projects">
                 <li onClick={() => setNav(false)} className="py-4 text-sm">
                   Projects
+                </li>
+              </Link>
+              <Link href="/#resume">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Resume
                 </li>
               </Link>
               <Link href="/#contact">
