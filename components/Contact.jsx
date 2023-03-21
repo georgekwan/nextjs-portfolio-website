dc7abe3e6907c033ec0fdeb7fc726ee1f741c608
George <gwmkwan@gmail.com>
2023-03-20 18:36:55 -0600

    feat: handle validation

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -14,6 +14,32 @@ const Contact = () => {
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
 
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (fullname.length <= 0) {
+      tempErrors['fullname'] = true;
+      isValid = false;
+    }
+    if (email.length <= 0) {
+      tempErrors['email'] = true;
+      isValid = false;
+    }
+    if (subject.length <= 0) {
+      tempErrors['subject'] = true;
+      isValid = false;
+    }
+    if (message.length <= 0) {
+      tempErrors['message'] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log('errors', errors);
+    return isValid;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
