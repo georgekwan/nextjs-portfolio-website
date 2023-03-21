4316985464b8cb2394b4ea399744756a8ebc0526
George <gwmkwan@gmail.com>
2023-03-20 19:10:53 -0600

    feat: add phone input

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -26,7 +26,7 @@ async function sendEmail(req, res) {
         <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
               </div>
               <div class="container" style="margin-left: 20px;margin-right: 20px;">
-              <h3>You've got a new mail from ${req.body.fullname}, their email is: ✉️${req.body.email} </h3>
+              <h3>You've got a new mail from ${req.body.fullname}, their email is: ✉️${req.body.email}, their phone is: ☎️${req.body.phone} </h3>
               <div style="font-size: 16px;">
               <p>Message:</p>
               <p>${req.body.message}</p>
