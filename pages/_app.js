5a647fc2e101cea3c2dce642f21dd8e51d2c7542
George <gwmkwan@gmail.com>
2023-12-17 16:09:38 -0700

    feat: import tracking script to pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import NavBar from '@/components/NavBar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/styles/globals.css';
+import PlerdyScript from './api/PlerdyScript';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    PlerdyScript();
+  }, []);
   return (
     <>
       <NavBar />
