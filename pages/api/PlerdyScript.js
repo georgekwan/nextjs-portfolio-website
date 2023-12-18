8bb311c9b8758fd662d8325c59a09bc06745ce6b
George <gwmkwan@gmail.com>
2023-12-18 16:51:47 -0700

    fix: bug fix in tracking script

--- pages/api/PlerdyScript.js
+++ pages/api/PlerdyScript.js
@@ -1,23 +1,25 @@
 export default function PlerdyScript() {
-  // Adding the Plerdy tracking code within the function
-  var _protocol =
-    'https:' == document.location.protocol ? ' https://' : ' http://';
+  const _protocol =
+    'https:' == document.location.protocol ? 'https://' : 'http://';
   const _site_hash_code = '64deaadeed87b36a303f733fdf231835';
   const _suid = 45702;
-  var plerdyScript = document.createElement('script');
+
+  const plerdyScript = document.createElement('script');
   plerdyScript.setAttribute('defer', '');
   plerdyScript.dataset.plerdymainscript = 'plerdymainscript';
   plerdyScript.src =
     'https://d.plerdy.com/public/js/click/main.js?v=' + Math.random();
-  var plerdymainscript = document.querySelector(
+
+  const plerdymainscript = document.querySelector(
     "[data-plerdymainscript='plerdymainscript']"
   );
   if (plerdymainscript) {
     plerdymainscript.parentNode.removeChild(plerdymainscript);
   }
+
   try {
     document.head.appendChild(plerdyScript);
-  } catch (t) {
-    console.log(t, 'unable to add script tag');
+  } catch (error) {
+    console.log(error, 'unable to add script tag');
   }
 }
