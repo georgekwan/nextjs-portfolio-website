d6ab110ced5cadafed6be168c3891110cce009e0
George <gwmkwan@gmail.com>
2023-12-18 16:57:54 -0700

    feat: add tracking function to main page

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
 
+import PlerdyScript from '@/pages/api/PlerdyScript';
+
 const Main = () => {
+  useEffect(() => {
+    PlerdyScript();
+  }, []);
   return (
     <div id="home" className="w-full h-screen text-center mx-2">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
