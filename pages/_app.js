a83134596d8bd73a484e7e1e7acd232be02cff0b
George <gwmkwan@gmail.com>
2023-12-19 14:42:23 -0700

    fix: bug fix on tracking script

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
 import { useEffect } from 'react';
+import Script from 'next/script';
 import NavBar from '@/components/NavBar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/styles/globals.css';
-import PlerdyScript from './api/PlerdyScript';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    PlerdyScript();
-  }, []);
   return (
     <>
+      <Script src="./api/PlerdyScript.js" />
       <NavBar />
       <Component {...pageProps} />
       <ToastContainer autoClose={3000} theme="colored" />
