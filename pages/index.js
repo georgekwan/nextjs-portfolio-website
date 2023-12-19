e545efabb47cc0f1cca4702914b1df836218029f
George <gwmkwan@gmail.com>
2023-12-18 19:23:38 -0700

    feat: add tracking script

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 
+import PlerdyScript from './api/PlerdyScript';
+
 export default function Home() {
+  useEffect(() => {
+    PlerdyScript();
+  }, []);
   return (
     <>
       <Head>
