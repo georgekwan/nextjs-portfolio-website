ff605dfb1a43d008dfbbed83fa0586c89463bc9f
George <gwmkwan@gmail.com>
2023-12-20 21:03:26 -0700

    chore: update meta tag

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>George Kwan | Software Developer</title>
+        <title>
+          George Kwan | Software Developer Specializing in Website Management,
+          Mobile App Development, and SEO Optimization
+        </title>
         <meta
           name="description"
-          content="I am a software Developer specializing in seamless web and mobile experiences."
+          content="I am a skilled software developer with expertise in website management, mobile app development, and SEO optimization. With experience as a Website Manager at Gateway Association Alberta, I have successfully maintained nonprofit websites by ensuring smooth functionality and implementing continuous updates. As a Mobile App Developer at New Idea Machine, I have built immersive mobile interfaces using JavaScript and React Native, while also mentoring others. My proficiency in optimizing SEO and metadata, as well as my ability to collaborate cross-functionally and resolve bugs efficiently, make me a valuable asset to any software development team."
         />
         <link rel="icon" href="/fav.png" />
       </Head>
