5bdd94d31d1aaa711c8c754a459c2a5200e67790
George <gwmkwan@gmail.com>
2024-02-13 13:05:35 -0700

    fix: ad sense code

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2987556127489545"
           crossorigin="anonymous"
-        ></script>
+        />
       </Head>
       <body>
         <Main />
