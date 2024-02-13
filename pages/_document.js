7d44184549675264d8314ae2fcbc5967cac3a9c5
George <gwmkwan@gmail.com>
2024-02-13 11:45:18 -0700

    feat: add ad sense script

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          data-ad-client="ca-pub-2987556127489545"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
