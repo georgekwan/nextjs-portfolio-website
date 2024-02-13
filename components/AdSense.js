79af1b85bfae9d5a196464b98abb44f9e3685f72
George <gwmkwan@gmail.com>
2024-02-13 11:42:37 -0700

    feat: create ad sense component

---
+++ components/AdSense.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const AdSense = ({ adSlot }) => {
+  useEffect(() => {
+    if (window) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-2987556127489545" // Replace with your publisher ID
+      data-ad-slot={adSlot}
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+};
+
+export default AdSense;
